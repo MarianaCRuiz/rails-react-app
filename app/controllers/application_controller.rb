e90af8d389dc2c4e6c5b2e9ec336642c7c387ab1
MARIANA_RUIZ <mchierandaruiz0@gmail.com>
2024-12-18 20:57:20 -0300

    add test request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  rescue_from ActiveRecord::RecordInvalid do |error|
-    render json: error.full_messages, status: :unprocessable_entity
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    render json: exception.record.errors.full_messages, status: :unprocessable_entity
   end
 end
--- spec/requests/courses_spec.rb
+++ spec/requests/courses_spec.rb
@@ -29,41 +29,79 @@ RSpec.describe "Courses", type: :request do
   end
 
   describe "POST /api/v1/courses" do
-    it "returns the new registered course" do
-      post "/api/v1/courses", params: {
-        course: {
-          registration_code: 'cs12',
-          name: 'Desenvolvimento e pesquisa',
-          description: 'segundo modulo do curso'
+    context 'success' do
+      it "returns the new registered course" do
+        post "/api/v1/courses", params: {
+          course: {
+            registration_code: 'cs12',
+            name: 'Desenvolvimento e pesquisa',
+            description: 'segundo modulo do curso'
+          }
         }
-      }
-      expect(response).to have_http_status(201)
-      expect(parsed_body).to include(
-        registration_code: "cs12",
-        name: "Desenvolvimento e pesquisa",
-        description: "segundo modulo do curso"
-      )
+        expect(response).to have_http_status(201)
+        expect(parsed_body).to include(
+          registration_code: "cs12",
+          name: "Desenvolvimento e pesquisa",
+          description: "segundo modulo do curso"
+        )
+      end
+    end
+
+    context 'failure' do
+      it "returns the errors messages" do
+        post "/api/v1/courses", params: {
+          course: {
+            registration_code: '',
+            name: nil,
+            description: nil
+          }
+        }
+        expect(response).to have_http_status(422)
+        expect(parsed_body).to eq(
+          [
+            "Registration code can't be blank",
+            "Name can't be blank",
+            "Description can't be blank"
+          ]
+        )
+      end
     end
   end
 
   describe "PUT /api/v1/courses/:id" do
-    it "returns the new registered course" do
-      course = create(:course, registration_code: 'cs11', name: 'Introdução a processos',
-      description: 'inicio do curso')
-      put "/api/v1/courses/#{course.id}", params: {
-        course: {
-          registration_code: 'cs12',
-          name: 'Desenvolvimento e pesquisa',
-          description: 'segundo modulo do curso'
+    context 'success' do
+      it "returns the new registered course" do
+        course = create(:course, registration_code: 'cs11', name: 'Introdução a processos',
+        description: 'inicio do curso')
+        put "/api/v1/courses/#{course.id}", params: {
+          course: {
+            registration_code: 'cs12',
+            name: 'Desenvolvimento e pesquisa',
+            description: 'segundo modulo do curso'
+          }
         }
-      }
-      expect(response).to have_http_status(200)
-      expect(parsed_body).to include(
-        id: course.id,
-        registration_code: "cs12",
-        name: "Desenvolvimento e pesquisa",
-        description: "segundo modulo do curso"
-      )
+        expect(response).to have_http_status(200)
+        expect(parsed_body).to include(
+          id: course.id,
+          registration_code: "cs12",
+          name: "Desenvolvimento e pesquisa",
+          description: "segundo modulo do curso"
+        )
+      end
+    end
+
+    context 'failure' do
+      it "returns the errors messages" do
+        course = create(:course, registration_code: 'cs11', name: 'Introdução a processos',
+        description: 'inicio do curso')
+        put "/api/v1/courses/#{course.id}", params: {
+          course: {
+            registration_code: ''
+          }
+        }
+        expect(response).to have_http_status(422)
+        expect(parsed_body).to eq(["Registration code can't be blank"])
+      end
     end
   end
 end
